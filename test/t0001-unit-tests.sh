8092c2e87737d05384ef5d93de563c57cc85a670
Randall S. Becker <randall.becker@nexbridge.ca>
2023-07-27 17:58:19 -0400

    Additional parameterization for GMake tests

--- test/t0001-unit-tests.sh
+++ test/t0001-unit-tests.sh
@@ -16,27 +16,27 @@ test_expect_success 'tandem_ext.h tests' '
 	${MAKE_SUBVOL_PREFIX_GUARDIAN_LC}${this_test}
 	${MAKE_SUBVOL_PREFIX_GUARDIAN_LC}${this_test}
 	
-	\$data02
-	\$data02.subvol
-	\$data02.subvol.file
-	\\node
-	\\node
-	\\node.\$data02
-	\\node.\$data02.subvol
-	\\node.\$data02.subvol.file
+	${TEST_DISK_GRD}
+	${TEST_DISK_GRD}.subvol
+	${TEST_DISK_GRD}.subvol.file
+	${TEST_NODE_GRD}
+	${TEST_NODE_GRD}
+	${TEST_NODE_GRD}.${TEST_DISK_GRD}
+	${TEST_NODE_GRD}.${TEST_DISK_GRD}.subvol
+	${TEST_NODE_GRD}.${TEST_DISK_GRD}.subvol.file
 	ToOSS:
 	(null)
 	/E
-	/G/data02
-	/G/data02/subvol
-	/G/data02/subvol/file
-	/E/node
-	/E/node
-	/E/node/G/data02
-	/E/node/G/data02/subvol
-	/E/node/G/data02/subvol/file
+	/G/${TEST_DISK_OSS}
+	/G/${TEST_DISK_OSS}/subvol
+	/G/${TEST_DISK_OSS}/subvol/file
+	/E/${TEST_NODE_OSS}
+	/E/${TEST_NODE_OSS}
+	/E/${TEST_NODE_OSS}/G/${TEST_DISK_OSS}
+	/E/${TEST_NODE_OSS}/G/${TEST_DISK_OSS}/subvol
+	/E/${TEST_NODE_OSS}/G/${TEST_DISK_OSS}/subvol/file
 	EOF
-	launch_helper "${TANDEM_EXT_HELPER}" node data02 >actual &&
+	launch_helper "${TANDEM_EXT_HELPER}" ${TEST_NODE_OSS} ${TEST_DISK_OSS} >actual &&
 	test_cmp expecting actual
 '
 
